89ffd700db7367e49614134d8e47e45d53065b1e
Arturo Gomez <arturogomezgz04@gmail.com>
2025-12-09 12:59:34 -0600

    Fetch laboratory data from external source and remove local JSON file

--- services/readLaboratorios.ts
+++ services/readLaboratorios.ts
@@ -1,7 +1,10 @@
 import Laboratorio from "../classes/Laboratorio"
-import data from "../src/assets/laboratorios.json"
 
-const laboratorios: Laboratorio[] = data.laboratorios.map((lab) =>
+const url = "https://gist.githubusercontent.com/ArturoGomezGz/e00b274d3be335a6d667a797c1244322/raw/b5b44da155c82ebb7f9c9367eb5d400167629437/data.json"
+
+const data = await fetch(url).then((res) => res.json())
+
+const laboratorios: Laboratorio[] = data.laboratorios.map((lab: Laboratorio) =>
   new Laboratorio(lab.nombre, lab.token)
 )
 
