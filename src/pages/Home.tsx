31e0d5fe61e27b3af4d8157c3b15a052b35745fb
Arturo Gomez <arturogomezgz04@gmail.com>
2025-12-08 23:31:46 -0600

    Add Home component layout and styling for laboratory cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom"
 import laboratorios from "../../services/readLaboratorios"
 import LaboratorioCard from "../../components/LaboratorioCard"
 
+import "./Home.css"
+
 export default function Home() {
     return (
-        <>
+        <div className="contenedor">
             {laboratorios.map((lab, index) =>
                 <Link key={index} to={`/laboratorio/${index}`}>
                     <LaboratorioCard lab={lab} />
                 </Link>
             )}
-        </>
+        </div>
     )
 }
